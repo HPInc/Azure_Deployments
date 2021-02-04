2553f09ca8bb9670c1fc3aa91725ef0e78bcaee6
michaeljacinto <atmwhockey@hotmail.com>
2021-02-04 13:14:41 -0800

    Add multi-region traffic manager deployment

---
+++ terraform-deployments/deployments/multi-region-traffic-manager/output.tf
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+output "resource_group" {
+  value = azurerm_resource_group.main.name
+}
+
+output "domain-controller-private-ip" {
+  value = module.dc-cac-network.dc-private-ip
+}
+
+output "domain-controller-public-ip" {
+  value = module.dc-cac-network.dc-public-ip
+}
+
+output "locations" {
+  value = module.dc-cac-network.subnet-workstation-locations
+}
+
+output "windows-standard-workstations" {
+  value = [for index, item in module.workstation-map.windows-std-workstations :
+    {
+      name : module.windows-std-vm.windows-std-vm-names[item.index],
+      private_ip : module.windows-std-vm.windows-std-vm-private-ips[item.index]
+    }
+  ]
+}
+
+output "windows-graphics-workstations" {
+  value = [for index, item in module.workstation-map.windows-gfx-workstations :
+    {
+      name : module.windows-gfx-vm.windows-gfx-vm-names[item.index],
+      private_ip : module.windows-gfx-vm.windows-gfx-vm-private-ips[item.index]
+    }
+  ]
+}
+
+output "centos-graphics-workstations" {
+  value = [for index, item in module.workstation-map.centos-gfx-workstations :
+    {
+      name : module.centos-gfx-vm.centos-gfx-vm-names[item.index],
+      private_ip : module.centos-gfx-vm.centos-gfx-vm-private-ips[item.index]
+    }
+  ]
+}
+
+output "centos-standard-workstations" {
+  value = [for index, item in module.workstation-map.centos-std-workstations :
+    {
+      name : module.centos-std-vm.centos-std-vm-names[item.index],
+      private_ip : module.centos-std-vm.centos-std-vm-private-ips[item.index]
+    }
+  ]
+}
+
+output "cac-vms" {
+  description = "Cac virtual machines"
+  value = [for index, item in module.cac-vm.cac-vm-locations :
+    {
+      name : module.cac-vm.cac-vm-names[index],
+      public_ip : module.cac-vm.cac-vm-public-ips[index],
+      private_ip : module.cac-vm.cac-vm-private-ips[index]
+    }
+  ]
+}
+
+output "traffic-manager-domain" {
+  description = "Traffic manager domain"
+  value       = module.traffic-manager.traffic-manager-id
+}
---
+++ terraform-deployments/deployments/multi-region-traffic-manager/required-vars.tf
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+variable "workstations" {
+  description = "List of workstation types to create"
+  type = list(object({
+    prefix         = string # The prefix applied to each VM name
+    location       = string # The region this set will be deployed in
+    workstation_os = string # The OS type for this VM "windows" or "linux"
+    vm_size        = string # Valid VM size for the region selected
+    disk_type      = string # Valid VM disk type
+    count          = number # The number of VMs of this type to create
+    disk_size      = number # The size of the VM disk in GB
+    isGFXHost      = bool   # High end graphics VM host indicator for the installation of graphics drivers
+  }))
+}
+
+variable "cac_configuration" {
+  description = "List of cac vrtual machines create"
+  type = list(object({
+    location  = string
+    cac_token = string
+  }))
+}
+
+variable "ad_admin_password" {
+  description = "Password for the Adminstrator user"
+}
+
+variable "safe_mode_admin_password" {
+  description = "Safe Mode Admin Password (Directory Service Restore Mode - DSRM)"
+  type        = string
+}
+
+variable "pcoip_registration_code" {
+  description = "PCoIP Registration code"
+  type        = string
+}
+
+variable "traffic_manager_dns_name" {
+  description = "The dns name of the traffic manager"
+  type        = string
+}
