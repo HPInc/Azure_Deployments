9976628c46929c2ae9f9f219d47c7de2f866c2c9
Bill Yan <byan@teradici.com>
2021-08-26 22:45:49 +0000

    changed to service principal authentication

--- terraform-deployments/deployments/casm-aadds/required-vars.tf
+++ terraform-deployments/deployments/casm-aadds/required-vars.tf
@@ -1,16 +1,13 @@
-variable "subscription_id" {
-  description = "Subscription ID of the directory the AADDS will be deployed in"
-  type        = string
-}
-
 variable "aadds_rg_name" {
   description = "Name of the resource group the AADDS will belong in"
   type        = string
+  default     = "BY_AAD_DS_Teradici"
 }
 
 variable "aadds_vnet_name" {
   description = "Name of the VNET the AADDS will belong in"
   type        = string
+  default     = "AAD_DS_TeraVNet"
 }
 
 variable "aadds_location" {
@@ -27,3 +24,12 @@ variable "pfx_cert_password" {
   description = "Password of the PFX certificate"
   type        = string
 }
+
+variable "application_id" {
+  type = string
+}
+
+variable "aad_client_secret" {
+  type = string
+}
+
