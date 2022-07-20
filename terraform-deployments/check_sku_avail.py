bb8460847d59fb0f432336ffb557621564bff953
Michael Cheung <michael.cheung@hp.com>
2022-07-20 10:42:28 -0700

    Parameterize deployment type WIP

--- terraform-deployments/check_sku_avail.py
+++ terraform-deployments/check_sku_avail.py
@@ -9,7 +9,7 @@
 # TODO: add more flexibiility to CAC and CAS Manager VM checks. Original creation of this script uses 
 # CAS Manager SaaS deployment var files.
 #
-# Last Updated: 07/15/2022
+# Last Updated: 07/18/2022
 
 import subprocess
 import time
@@ -30,6 +30,20 @@ REGIONS=["eastus", "eastus2", "southcentralus", "westus2", "westus3", "australia
          "southindia", "westindia", "canadaeast", "francesouth", "germanynorth", "norwaywest", "switzerlandwest", "ukwest",
          "uaecentral", "brazilsoutheast"]
 
+# List of deployment offerings
+DEPLOYMENTS=["cas-mgr-load-balancer-one-ip-nat",
+             "cas-mgr-one-ip-traffic-mgr",
+             "cas-mgr-single-connector",
+             "casm-aadds",
+             "casm-aadds-one-ip-lb",
+             "casm-aadds-one-ip-traffic-manager",
+             "casm-aadds-single-connector",
+             "lls-single-connector",
+             "load-balancer-one-ip",
+             "multi-region-traffic-mgr-one-ip",
+             "quickstart-single-connector",
+             "single-connector"]
+
 # General logging of process
 LOG_FILE = "sku_availability_check.log"
 
@@ -46,6 +60,10 @@ CAC_VM_SIZE_SET_FILE = "./modules/cac/cac-vm/main.tf"
 CASM_VM_SIZE_SET_FILE = "./modules/cas-mgr/main.tf"
 DC_VM_SIZE_SET_FILE = "./modules/dc/dc-vm/main.tf"
 
+# Priority list of possible SKU sizes for Windows/Linux Standard Workstations (NOTE: prepend selected terraform.tfvars size to this list when checking availability)
+STANDARD_SKUS_PRIORITY = ["Standard_B2s", "Standard_B2ms", "Standard_D2s_v3", "Standard_B4ms", "Standard_DS2_v2", "Standard_D4s_v3", "Standard_B8ms", "Standard_DS3_v2"]
+GFX_SKUS_PRIORITY = ["Standard_NV4as_v4", "Standard_NV6"]
+
 class MissingLocationError(Exception):
     pass
 
@@ -61,28 +79,22 @@ def log(msg):
     temp.close()
     print(msg)
 
-def extract_default_sizes(vm_type):
-    SIZE_FILE = None
-    VAR_LINE = ""
+
+def extract_default_sizes(vm_type, extract_file):
+    VAR_LINES = []
 
     if vm_type == "CAC":
-        SIZE_FILE = DEFAULT_CAC_VM_SIZE_FILE
-        VAR_LINE = "variable \"machine_type\""
+        VAR_LINES = ["variable \"machine_type\"", "variable \"cac_machine_type\""]
     if vm_type == "CAS Manager":
-        SIZE_FILE = DEFAULT_CASM_VM_SIZE_FILE
-        VAR_LINE = "variable \"machine_type\""
+        VAR_LINES = ["variable \"machine_type\""]
     if vm_type == "DC":
-        SIZE_FILE = DEFAULT_DC_VM_SIZE_FILE
-        VAR_LINE = "variable \"dc_machine_type\""
+        VAR_LINES = ["variable \"dc_machine_type\""]
 
-    default_size_file = open(SIZE_FILE, 'r')
+    default_size_file = open(extract_file, 'r')
     in_var_block = False
     vm_sizes = []
 
     for line in default_size_file:
-        if line.find(VAR_LINE) != -1:
-            in_var_block = True
-            continue
         if line.find("default") != -1 and in_var_block and (line.find("#") == -1 or line.find("#") > line.find("default")):
             vm_sizes = line[line.index("[")+1:line.index("]")].replace("[", "").replace("]", "").replace(", ", " ").replace("\"", "").split(" ")
             log("Default values found for " + vm_type + ": " + str(vm_sizes))
@@ -90,7 +102,23 @@ def extract_default_sizes(vm_type):
         if line.find("\}") != -1 and in_var_block:
             log("Did not find any set values for the " + vm_type + ".")
             raise MissingDefaultSizesError()
+
+        if not in_var_block:
+            for i in range(len(VAR_LINES)):
+                if line.find(VAR_LINES[i]) != -1:
+                    in_var_block = True
+                    break
     return vm_sizes
+
+
+def update_sku_selection(vm_type, location, extract_file, edit_file):
+    log("Finding default SKU sizes for " + vm_type + " VM...")
+    sizes = extract_default_sizes(vm_type, extract_file)
+    log("Checking availability of default " + vm_type + " VM sizes...")
+    check_sku_sizes(sizes, location)
+    available_sku_idx = determine_sku_size(vm_type, sizes)
+    log("Setting VM size index for " + vm_type + " VM...")
+    set_sku_size(edit_file, available_sku_idx)
 
 
 def check_sku_sizes(vm_sizes, location):
@@ -111,21 +139,26 @@ def determine_sku_size(vm_type, vm_sizes):
 
     log("Determining available size for " + vm_type + " under current Azure subscription...")
     for i in range(len(vm_sizes)):
+        sku_file.seek(0)
         for line in sku_file:
             if line.find(vm_sizes[i]) != -1:
                 if line.find("NotAvailableForSubscription") != -1:
-                    log("SKU size " + vm_sizes[i] + " is not available for this subscription")
+                    log("SKU size " + vm_sizes[i] + " is not available for this subscription in the current location")
                     break
                 sku_file.close()
                 log("SKU size " + vm_sizes[i] + " is highest priority for " + vm_type + " that is available")
                 return i
     
     log("SKU sizes for " + vm_type + " did not return any information. Please check the list of sizes requested for this component or the region to search.")
+    sku_file.close()
     raise InvalidSKUSizeError()
 
+
 def set_sku_size(set_file, idx):
-    edit_file = open(set_file, "r")
+    edit_file = open(set_file, "r+")
     replacement = ""
+
+    line_len = None
     for line in edit_file:
         changes = re.sub('var.machine_type\[[0-9]*\]', 'var.machine_type[' + str(idx) + ']', line)
         changes_cac = re.sub('var.cac_machine_type\[[0-9]*\]', 'var.cac_machine_type[' + str(idx) + ']', line)
@@ -139,58 +172,87 @@ def set_sku_size(set_file, idx):
             continue
         replacement += changes
         
+    edit_file.seek(0)
+    edit_file.write(replacement)
     edit_file.close()
 
-    edit_filewrite = open(set_file, "w")
-    edit_filewrite.write(replacement)
-    edit_filewrite.close()
-
     log("File edit complete. Changes may be verified in " + set_file)
+
+# TODO: set files to edit
+def assign_filenames(deployment):
+    if deployment == "cas-mgr-single-connector":
+        # DEFAULT_CAC_VM_SIZE_FILE = "./modules/cac/cac-vm/default-vars.tf"
+        # DEFAULT_CASM_VM_SIZE_FILE = "./modules/cas-mgr/vars.tf"
+        # DEFAULT_DC_VM_SIZE_FILE = "./modules/dc/dc-vm/default-vars.tf"
+        # CAC_VM_SIZE_SET_FILE = "./modules/cac/cac-vm/main.tf"
+        # CASM_VM_SIZE_SET_FILE = "./modules/cas-mgr/main.tf"
+        # DC_VM_SIZE_SET_FILE = "./modules/dc/dc-vm/main.tf"
+        pass
+    if deployment == "cas-mgr-load-balancer-one-ip-nat":
+        pass
+    if deployment == "cas-mgr-one-ip-traffic-mgr":
+        pass
+    if deployment == "casm-aadds":
+        pass
+    if deployment == "casm-aadds-one-ip-lb":
+        pass
+    if deployment == "casm-aadds-one-ip-traffic-manager":
+        pass
+    if deployment == "casm-aadds-single-connector":
+        pass
+    if deployment == "lls-single-connector":
+        pass
+    if deployment == "load-balancer-one-ip":
+        pass
+    if deployment == "multi-region-traffic-mgr-one-ip":
+        pass
+    if deployment == "quickstart-single-connector":
+        pass
+    if deployment == "single-connector":
+        pass
+
 
 def main():
     # Define parameters
     # - location (required) : the region in which to check for SKU sizes; this should be specified as the same as the desired workstations' region
     # - no_casm_vm          : whether or not the deployment will include CAS Manager as a separate VM; specify when indicating using CASM SaaS or other
     # - no_ldc              : whether or not the deployment will require a Local Domain Controller; specify when indicating using Azure AD Domain Services
+    # - deployment          : the specific deployment type intended to be checked for future use; overrides no_casm_vm and no_ldc variables
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--location", metavar="AAA", choices=REGIONS, required=True, help="Region to check for VM sizes")
     parser.add_argument("--no-casm-vm", default=False, dest="no_casm_vm", 
                         action='store_true', help="Flag for CAS Manager VM; True when using SaaS")
     parser.add_argument("--no-ldc", default=False, dest="no_ldc", action='store_true', help="Flag for DC VM; True when using AADDS")
+    parser.add_argument("-d", "--deployment", dest="deployment", metavar="BBB", choices=DEPLOYMENTS, help="Specific deployment option, overrides --no-casm-vm and --no-ldc")
     args = parser.parse_args()
+
+    assign_filenames(args.deployment)
 
     open(VM_AVAILABILITY_FILE, "a+").close()
 
     log("Querying for SKU sizes in location \"" + args.location + "\"")
     # TODO: should be taking from cac/cac-vm/
     # ... this varies depending on the deployment type, should we add a param for deployment types
-    log("Finding default SKU sizes for CAC-VM...")
-    cac_sizes = extract_default_sizes("CAC")
-    log("Checking availability of default CAC VM sizes...")
-    check_sku_sizes(cac_sizes, args.location)
-    available_sku_idx = determine_sku_size("CAC", cac_sizes)
-    log("Setting VM size index for CAC VM...")
-    set_sku_size(CAC_VM_SIZE_SET_FILE, available_sku_idx)
+    update_sku_selection("CAC", args.location, DEFAULT_CAC_VM_SIZE_FILE, CAC_VM_SIZE_SET_FILE)
 
-    if not args.no_casm_vm:
-        # TODO: should be going from modules/cas-mgr-* based on deployment
-        log("Finding default SKU sizes for CAS Manager VM...")
-        casm_sizes = extract_default_sizes("CAS Manager")
-        log("Checking availability of default CAS Manager VM sizes...")
-        check_sku_sizes(casm_sizes, args.location)
-        available_sku_idx = determine_sku_size("CAS Manager", casm_sizes)
-        log("Setting VM size index for CAS Manager VM...")
-        set_sku_size(CASM_VM_SIZE_SET_FILE, available_sku_idx)
+    if not args.deployment:
+        if not args.no_casm_vm:
+            # TODO: should be going from modules/cas-mgr-* based on deployment
+            update_sku_selection("CAS Manager", args.location, DEFAULT_CASM_VM_SIZE_FILE, CASM_VM_SIZE_SET_FILE)
+            
 
-    if not args.no_ldc:
-        log("Finding default SKU sizes for Domain Controller VM...")
-        dc_sizes = extract_default_sizes("DC")
-        log("Checking availability of default Domain Controller VM sizes...")
-        check_sku_sizes(dc_sizes, args.location)
-        available_sku_idx = determine_sku_size("DC", dc_sizes)
-        log("Setting VM size index for Domain Controller...")
-        set_sku_size(DC_VM_SIZE_SET_FILE, available_sku_idx)
+        if not args.no_ldc:
+            update_sku_selection("DC", args.location, DEFAULT_DC_VM_SIZE_FILE, DC_VM_SIZE_SET_FILE)
+
+    else:
+        # Time to check the terraform.tfvars of the deployment to check for workstation-selected SKUs
+        log("Selected deployment type: " + args.deployment)
+        log("Determining selected workstation SKUs for your deployment. Corresponding directory must contain a terraform.tfvars file and it should be renamed to remove the .sample extension.")
+        tfvars_file = open("./deployments/" + args.deployment + "/terraform.tfvars", "r")
+
+        tfvars_file.close()
+
 
     subprocess.run(["rm", VM_AVAILABILITY_FILE])
     log("SKU availability check completed.\n")
